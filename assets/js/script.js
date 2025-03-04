6c1b3fd37acfd00d03c3355843d8dd055bbed882
SREENIVASULU RAMANABOINA <112157011+Sreenu827@users.noreply.github.com>
2025-03-04 07:54:53 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
 
+        // Smooth scroll to the section
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
@@ -13,10 +14,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 window.onload = () => {
     const sections = document.querySelectorAll('.home-section, .about-section, .projects-section, .contact-section');
     sections.forEach((section, index) => {
+        // Initially set opacity to 0 for each section
         section.style.opacity = 0;
         section.style.transition = 'opacity 1s ease-in-out';
+
+        // Add a timeout to create a delay between animations for each section
         setTimeout(() => {
             section.style.opacity = 1;
-        }, 500 * (index + 1));
+        }, 500 * (index + 1)); // The delay increases with each section
     });
 };
