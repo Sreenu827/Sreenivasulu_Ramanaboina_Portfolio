d6f7570a97c42750b23ad71476af2f2e1a898304
SREENIVASULU RAMANABOINA <112157011+Sreenu827@users.noreply.github.com>
2025-03-04 07:52:11 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,11 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 // Animation effect for sections on page load
 window.onload = () => {
     const sections = document.querySelectorAll('.home-section, .about-section, .projects-section, .contact-section');
-    sections.forEach(section => {
+    sections.forEach((section, index) => {
         section.style.opacity = 0;
         section.style.transition = 'opacity 1s ease-in-out';
         setTimeout(() => {
             section.style.opacity = 1;
-        }, 500);
+        }, 500 * (index + 1));
     });
 };
